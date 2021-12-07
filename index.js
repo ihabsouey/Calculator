910860fe11045b99569c24e0833c588bc2cfb74d
ihab souey <57112177+ihabsouey@users.noreply.github.com>
2021-12-07 17:35:21 +0100

    final

---
+++ index.js
@@ -0,0 +1,19 @@
+var rslt = document.getElementById("input")
+function erease(){
+rslt.value= "";
+}
+function display(value){
+    rslt.value += value
+}
+function result(){
+    try {
+    var r = rslt.value
+    rslt.value=eval(r)}
+    catch {
+        alert("erreur dans la saisie ")
+    }
+}
+function del(){
+    var r = rslt.value
+    rslt.value= r.slice(0,r.length-1)
+}
